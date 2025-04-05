ced17bf41ac93d9c350513e6ee0ef0e64770d3ec
Vivi <100116566+DeadpoolDoingDishes@users.noreply.github.com>
2025-04-05 18:43:11 +0200

    Delete src directory

--- src/main/java/com/quizbot/repository/PointsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quizbot.repository;
-
-import com.quizbot.model.Points;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PointsRepository extends JpaRepository<Points, String> {
-} 
